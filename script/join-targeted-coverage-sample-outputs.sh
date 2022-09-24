7d219c90b9ef1acd7eef60b2a5e35b8b4b8f179a
Nicole Zeltser <nicole.zeltser@gmail.com>
2022-09-23 18:18:24 -0700

    use command line args

--- script/join-targeted-coverage-sample-outputs.sh
+++ script/join-targeted-coverage-sample-outputs.sh
@@ -3,10 +3,10 @@
 # recursive function for joining single sample outputs of pipeline-targeted-coverage
 # ie bed files with a fourth column
 
-# Usage: ./join-targeted-coverage-sample-outputs pathfile
+# Usage: bash join-targeted-coverage-sample-outputs [pathfile] [output]
 # where pathfile = a list of filenames to join. 
 # The file names must be separated by NUL (like the output produced by the command "find ... -print0").
 
 sort -k1,1 -k2,2n -s --files0-from $1 | \
 mergeBed -c 4 -o collapse -delim DELIM | \
-sed 's/DELIM/\t/g'
+sed 's/DELIM/\t/g' > $2
