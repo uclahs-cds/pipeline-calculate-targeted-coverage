0e8c809f2478eedb1b68cafedb060633319f3313
Nicole Zeltser <nicole.zeltser@gmail.com>
2022-09-29 17:27:18 -0700

    extra documentation

--- script/join-targeted-coverage-sample-outputs.sh
+++ script/join-targeted-coverage-sample-outputs.sh
@@ -6,6 +6,11 @@
 # where pathfile = a list of filenames to join. 
 # The file names must be separated by NUL (like the output produced by the command "find ... -print0").
 
+# Output columns are in the order in which they are inputed, so make sure input paths are sorted
+# accordingly (find outputs are typically in order of file creation):
+
+# find /path/to/targeted-coverage/output/dir/ -name "*.collapsed_coverage.bed" | sort | tr '\n' '\0'
+
 sort -k1,1 -k2,2n -s --files0-from $1 | \
 mergeBed -c 4 -o collapse -delim DELIM | \
 sed 's/DELIM/\t/g' > $2
